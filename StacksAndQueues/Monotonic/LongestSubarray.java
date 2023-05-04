63507d14a1e34ce05f8d1a7ce661667244a6782f
terrencejihoonjung <terrencejung928@gmail.com>
2023-05-04 15:16:54 -0400

    monotonic

--- StacksAndQueues/Monotonic/LongestSubarray.java
+++ StacksAndQueues/Monotonic/LongestSubarray.java
@@ -3,6 +3,36 @@ package StacksAndQueues.Monotonic;
 
 class Solution {
     public int longestSubarray(int[] nums, int limit) {
-        
+        ArrayDeque<Integer> increasing = new ArrayDeque<>();
+        ArrayDeque<Integer> decreasing = new ArrayDeque<>();
+        int ans = 0;
+        int left = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            while (!increasing.isEmpty() && increasing.getLast() > nums[right]) {
+                increasing.removeLast();
+            }
+            while (!decreasing.isEmpty() && decreasing.getLast() < nums[right]) {
+                decreasing.removeLast();
+            }
+
+            increasing.addLast(nums[right]);
+            decreasing.addLast(nums[right]);
+
+            // Maintaining window constrant
+            while (decreasing.getFirst() - increasing.getFirst() > limit) {
+                if (nums[left] == increasing.getFirst()) {
+                    increasing.removeFirst();
+                }
+                if (nums[left] == decreasing.getFirst()) {
+                    decreasing.removeFirst();
+                }
+                left++;
+            }
+
+            ans = Math.max(ans, right - left + 1);
+        }
+
+        return ans;
     }
 }
